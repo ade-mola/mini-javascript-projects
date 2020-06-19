e3673c38c3eada7bdbb2e4b7f250b3bd1c44f457
ade-mola <salamiolokun@gmail.com>
2020-06-19 13:33:38 +0100

    added mutate method

--- mystery-organism-starter/main.js
+++ mystery-organism-starter/main.js
@@ -13,6 +13,36 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+// Factory function to create objects.
+const pAequorFactory = (number, dnaBases) => {
+  return {
+    specimenNum: number,  // the specimen number with no two orgnasims having same number
+    dna: dnaBases,        // array of 15 dna bases 
+
+    // Mutates a random base in the dna strand to a new random base
+    mutate () {
+      const randomIndex = Math.floor(Math.random() * this.dna.length);
+      const generatedBase = returnRandBase();
+
+      if (this.dna[randomIndex] === generatedBase) {``
+        console.log(`New DNA base '${generatedBase}' is identical to current base '${this.dna[randomIndex]}'`);
+        return this.dna;
+      } else {
+        console.log(`----------------\nOriginal DNA base: ${this.dna[randomIndex]} at index: ${randomIndex}`);
+        this.dna[randomIndex] = generatedBase;
+        console.log(`Newly inserted DNA base: ${this.dna[randomIndex]} at index: ${randomIndex}\n----------------\n`);
+        return this.dna;
+      }
+    }
+  }
+};
+
+let test = pAequorFactory(1, mockUpStrand());
+// console.log(test)
+console.log(test.dna)
+test.mutate()
+console.log(test.dna);
+
 
 
 
